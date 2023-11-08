6f1c780f71d3c2c31226aea696f8f5cb3cddf940
lpossanzini <lpossanzini@teco.com.ar>
2023-11-07 22:45:48 -0300

    Se agrega un cambio

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 
 app.get("/", (req, res) =>{
-    res.send("Hola desde express")
+    res.send("<h1>HOLAAA</h1>")
 })
 app.listen(3000, ()=>{
     console.log("Server on")
 })
+ 
